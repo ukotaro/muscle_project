5c0483e0a8d6b18a23cbfb7b49d518c27ddb190c
ukotaro <umedakotaro1209@gmail.com>
2023-06-10 16:38:47 +0900

    順位と重量コンポーネント Fixes #20

---
+++ components/uiParts/RankingWeight.tsx
@@ -0,0 +1,35 @@
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, View, Text, Button, Image } from "react-native"
+import React, { useEffect, useState } from "react"
+
+type Props = {
+  rank: number
+  name: string
+  weight: number
+  uri:string
+}
+export default function RankingWeight({rank,name,weight,uri}:Props) {
+  return (
+    <View>
+      <View>
+        <Text style={styles.title}>{rank}位　{name}</Text>
+        <Text style={styles.weight}>総重量　{weight} kg</Text>
+      </View>
+      <Image source={{ uri: uri }} style={{ width: 250, height: 250 }} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize:25,
+    color:"#594639",
+    marginBottom:8,
+    fontWeight:"bold"
+  },
+  weight :{
+    fontSize:25,
+    color:"#594639",
+    marginBottom:8,
+  }
+})
