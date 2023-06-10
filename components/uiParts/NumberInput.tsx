aba6ccd9d3e718e9a8342aa0a72cd33cb4631a8e
yui645 <kirua1313@gmail.com>
2023-06-10 16:38:21 +0900

    数値入力コンポーネント Fixes #21

---
+++ components/uiParts/NumberInput.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  SafeAreaView,
+  TextInput,
+  StyleSheet,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+const NumberInput = () => {
+  const [number, onChangeNumber] = React.useState("");
+  //使いたい部分をTouchableで囲う必要あり
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder=""
+          keyboardType="numeric"
+        />
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    width: 80,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default NumberInput;
