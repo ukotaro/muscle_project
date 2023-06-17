2148689f24dd69f9002cd27a3b1a845f358a6bc1
ukotaro <umedakotaro1209@gmail.com>
2023-06-17 14:25:29 +0900

    カメラ実装 Fixes #93

--- screens/Camera.tsx
+++ screens/Camera.tsx
@@ -1,12 +1,13 @@
 import { Camera, CameraType } from "expo-camera"
 import React, { useState } from "react"
 import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native"
-import { MaterialCommunityIcons, AntDesign, FontAwesome5 ,Ionicons} from "@expo/vector-icons"
+import { MaterialCommunityIcons, AntDesign, FontAwesome5, Ionicons } from "@expo/vector-icons"
 
 export default function CameraShot() {
   const [type, setType] = useState(CameraType.back)
   const [permission, requestPermission] = Camera.useCameraPermissions()
-  const [camera,setCamera] = useState(null)
+  const [camera, setCamera] = useState(null)
+  const [image, setImage] = useState<string | null>(null)
 
   if (!permission) {
     // Camera permissions are still loading
@@ -26,19 +27,33 @@ export default function CameraShot() {
   function toggleCameraType() {
     setType((current) => (current === CameraType.back ? CameraType.front : CameraType.back))
   }
+  const takePicture = async () => {
+    if (camera) {
+      const image = await camera.takePictureAsync({})
+      setImage(image.uri)
+      console.log(image)
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
+      <Camera
+        style={styles.camera}
+        type={type}
+        ref={(ref) => {
+          setCamera(ref)
+        }}
+      >
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={{
-              width: 40,
-              height: 40,
+              width: 60,
+              height: 60,
               borderRadius: 50,
               borderWidth: 5,
               borderColor: "#594639",
             }}
+            onPress={takePicture}
           />
           <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
             <Ionicons size={60} color="#594639" name="camera-reverse" />
@@ -60,10 +75,11 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flex: 1,
     flexDirection: "row",
-    alignItems:"flex-end",
+    alignItems: "flex-end",
     backgroundColor: "transparent",
-    justifyContent:"space-between",
+    justifyContent: "space-evenly",
     margin: 64,
+    marginLeft:100
   },
   button: {
     flex: 1,
