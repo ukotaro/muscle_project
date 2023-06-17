52591cc6214c221273634754f2217f8b187b6fe9
ryunosuke121 <ryunosuke.ito@horizon-cg.com>
2023-06-17 11:11:04 +0900

    post_api修正

--- utils/post_api.ts
+++ utils/post_api.ts
@@ -1,12 +1,20 @@
 import axios from "axios";
 import { Training, Post } from "../type";
-import { exp } from "react-native-reanimated";
+//import { Platform } from "react-native";
+import mime from 'react-native-mime-types';
+import RNFetchBlob from 'rn-fetch-blob';
+
+
+const api = axios.create({
+    baseURL: "http://18.233.93.14:8080",
+})
 
 // タイムライン投稿の作成
 // トレーニングを同時に作成するには、トレーニングの情報を引数に追加する
 // 既存のトレーニングを使う場合は、training_idを引数に追加する
-export const createPost = async (user_id: number, comment: string, training_id?: number, training?: Training, imageFile?: Blob) => {
+export const createPost = async (user_id: number, comment: string, training_id?: number, training?: Training, imageUri?: string) => {
     const formData = new FormData();
+
     let post_info = {
         user_id: user_id,
         training_id: training_id,
@@ -14,21 +22,42 @@ export const createPost = async (user_id: number, comment: string, training_id?:
         comment: comment
     }
     formData.append('post_info', JSON.stringify(post_info));
-    formData.append('post_image', imageFile);
-
-    try {
-        const res = await axios.post<{ message: string, data: Post }>(`/post`, formData);
-        return res.data.data;
-    } catch (err) {
-        console.log(err);
-        return "api error"
+    if (imageUri) {
+        RNFetchBlob.fetch('POST', 'http://18.233.93.14:8080/post', {
+            'Content-Type': 'multipart/form-data',
+        }, [
+            { name: 'post_info', data: JSON.stringify(post_info) },
+            { name: 'post_image', filename: imageUri, type: mime.lookup(imageUri), data: RNFetchBlob.wrap(imageUri) }
+        ]).then((res) => {
+            console.log(res);
+            return res.data.data;
+        }).catch((err) => {
+            console.log(err);
+            return "api error"
+        })
+    } else {
+        formData.append('post_image', '');
+        try {
+            const res = await api<{ message: string, data: Post }>({
+                url: '/post',
+                data: formData,
+                method: 'post',
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            return res.data.data;
+        } catch (err) {
+            console.log(err);
+            return "api error"
+        }
     }
 }
 
 // ユーザーのタイムライン投稿を全て取得
 export const getUserPosts = async (user_id: number) => {
     try {
-        const res = await axios.get<{ message: string, data: Post[] }>(`/user/posts/${user_id}`);
+        const res = await api.get<{ message: string, data: Post[] }>(`/user/post/${user_id}`);
         return res.data.data;
     } catch (err) {
         console.log(err);
@@ -36,4 +65,11 @@ export const getUserPosts = async (user_id: number) => {
     }
 }
 
+createPost(1, "test", 1, undefined, '/Users/itouryuunosuke/Project/muscle_project/o0320048214003980269-1-1.jpeg').then((res) => {
+    console.log(res);
+})
+
+// getUserPosts(1).then((res) => {
+//     console.log(res);
+// })
 // TODO:トレーニングメニュー一覧の取得
